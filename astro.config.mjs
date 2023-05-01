b8c829d3610606d0385b1965381d412b6a89801e
mahima98 <mahimaramgolam98@gmail.com>
2023-05-01 21:36:00 +0400

    added astro auth config and package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import auth from 'auth-astro';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    auth(),
+  ],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});

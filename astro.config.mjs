292be3960e7436807a43e9b5ced66c71e3f7cdc8
mahima98 <mahimaramgolam98@gmail.com>
2023-05-01 21:47:42 +0400

    text deploy #03

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 // import auth from 'auth-astro';
 
 export default defineConfig({
   integrations: [
     // auth(),
   ],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  // output: "server",
+  // adapter: node({
+  //   mode: "standalone"
+  // })
 });
